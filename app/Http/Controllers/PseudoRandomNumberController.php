9cb9358ea973468a9875c0bf7c9539323cdff9a2
Augusto Lescano <au6usto@gmail.com>
2020-06-02 17:44:45 -0300

    Corrijo algoritmo de generacion de numeros, agrego pruebas

---
+++ app/Http/Controllers/PseudoRandomNumberController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\PseudoRandomNumber;
+
+class PseudoRandomNumberController extends Controller
+{
+    public function getNumbers($seed, $amount) {
+        $rn = new PseudoRandomNumber($seed, $amount);
+        $rn->generate();
+        // $rn->test(43);
+
+        return $rn->getList();
+    }
+}
--- app/PseudoRandomNumber.php
+++ app/PseudoRandomNumber.php
@@ -4,33 +4,40 @@ namespace App;
 
 class PseudoRandomNumber
 {
+    const MODULUS = 8;
+    const MULTIPLIER = 3;
+    const ADITIVE = 5;
+
     private $seed;
+    private $amount;
+    private $randomNumber;
+    private $randomNumberList;
 
-    private $randomList;
-
-    private $number;
-
-    private $multiplier;
-
-    private $aditive;
-
-    private $modulus;
-
-    private $iterations;
-
-    public function __construct($seed, $multiplier, $aditive, $modulus) {
+    public function __construct($amount, $seed) {
         $this->seed = $seed;
-        $this->number = $number;
-        $this->multiplier = $multiplier;
-        $this->aditive = $aditive;
-        $this->modulus = $modulus;
+        $this->randomNumber = $seed;
+        $this->amount = $amount;
+        $this->randomNumberList = collect([]);
     }
 
-    public function generate() : array {
-        for ($i=0; $i <= $iterations ; $i++) {
-            $this->randomNumberList[$i]['n'] = ($this->multiplier * $this->number + $this->aditive) % $this->modulus;
-            $this->randomNumberList[$i]['u'] = $thi->number / $this->modulus;
+    public function generate() : void {
+        for ($i=0; $i <= $this->amount ; $i++) {
+            $this->randomNumber = (self::MULTIPLIER * $this->randomNumber + self::ADITIVE) % self::MODULUS;
+            $this->randomNumberList->push([
+                'n' => $this->randomNumber,
+                'u' => $this->randomNumber / self::MODULUS
+            ]);
         }
-        return $this->randomNumberList;
+    }
+
+    public function getList() : array {
+        return $this->randomNumberList->toArray();
+    }
+
+    public function testNumbers($z) : bool {
+        $n = $this->randomNumberList->count();
+        $mean = $this->randomNumberList->sum('u') / $n;
+        $statZ = (($mean - 0.5) * sqrt($n)) / sqrt(1/12);
+        return abs($statZ) < $z;
     }
 }
