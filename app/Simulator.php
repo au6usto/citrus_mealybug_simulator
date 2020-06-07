84de670c2690f6479c0a02a6275d649163462a8d
Augusto Lescano <au6usto@gmail.com>
2020-06-07 00:56:07 -0300

    Corrijo nombres de datos de vectores de constructor

--- app/Simulator.php
+++ app/Simulator.php
@@ -31,44 +31,44 @@ class Simulator
 
         //Temperaturas por mes
         $this->temperaturePerMonth = collect([
-            10 => [15, 29],
-            11 => [18, 31],
-            12 => [20, 32],
-            1 => [22,32],
-            2 => [21, 30],
-            3 => [18, 29],
-            4 => [14, 26],
-            5 => [10, 23],
+            10 => ['min' => 15, 'max' => 29],
+            11 => ['min' => 18, 'max' => 31],
+            12 => ['min' => 20, 'max' => 32],
+            1 => ['min' => 22,'max' => 32],
+            2 => ['min' => 21, 'max' => 30],
+            3 => ['min' => 18, 'max' => 29],
+            4 => ['min' => 14, 'max' => 26],
+            5 => ['min' => 10, 'max' => 23],
         ]);
 
         //Vuelo de Machos por mes
         $this->maleFlightPerMonth = collect([
-            10 => [237.6, 84.1],
-            11 => [1529.2, 384.2],
-            12 => [1529.2, 384.2],
-            1 => [1251.6, 213.4],
-            2 => [404.4, 77.5],
-            3 => [228.04, 60.5]
+            10 => ['mean' => 237.6, 'deviation' => 84.1],
+            11 => ['mean' => 1529.2, 'deviation' => 384.2],
+            12 => ['mean' => 1529.2, 'deviation' => 384.2],
+            1 => ['mean' => 1251.6, 'deviation' => 213.4],
+            2 => ['mean' => 404.4, 'deviation' => 77.5],
+            3 => ['mean' => 228.04, 'deviation' => 60.5]
         ]);
 
         //Frutos ocupados por mes
         $this->occupiedFruitPerMonth = collect([
-            11 => [13.14, 3.9795],
-            12 => [13.14, 3.9795],
-            1 => [5.635, 22.545],
-            2 => [0.0005, 0.215]
+            11 => ['mean' => 13.14, 'deviation' => 3.9795],
+            12 => ['mean' => 13.14, 'deviation' => 3.9795],
+            1 => ['mean' => 5.635, 'deviation' => 22.545],
+            2 => ['mean' => 0.0005, 'deviation' => 0.215]
         ]);
 
         //Insectos en cáliz por mes
         $this->insectsInCalyxPerMonth = collect([
-            11 => [0.0215, 0.9913],
-            12 => [0.0215, 0.9913],
-            1 => [0.0002, 0.0892],
-            2 => [0.008, 13.806]
+            11 => ['mean' => 0.0215,'deviation' => 0.9913],
+            12 => ['mean' => 0.0215,'deviation' => 0.9913],
+            1 => ['mean' => 0.0002,'deviation' => 0.0892],
+            2 => ['mean' => 0.008,'deviation' => 13.806]
         ]);
     }
 
-    //Simular 12 meses
+    //Simular 4 períodos
     public function simulate()
     {
         foreach ($this->periods as $key => $period) {
@@ -76,10 +76,12 @@ class Simulator
             if ($period === 12) {
                 $simulation = $this->simulatedPeriodsList->get($key - 1);
             } else {
-                $maleQtyFromPeriod = $this->maleFlightPerMonth->get($this->month) ?? [0, 0];
+                $maleQtyFromPeriod = $this->maleFlightPerMonth->get($this->month);
                 $femaleMaleRatio = $this->femaleMaleRatio();
 
-                $maleQty = $this->probability->getNormal($maleQtyFromPeriod[0], $maleQtyFromPeriod[1]);
+                $maleQty = isset($maleQtyFromPeriod['mean']) ?
+                    $this->probability->getNormal($maleQtyFromPeriod['mean'], $maleQtyFromPeriod['deviation']) :
+                    0;
                 $femaleQty = $this->calculateFemaleQty();
                 $larvaeQty = $this->calculateLarvaeQty();
                 //Cantidad de Larvas Hembra
@@ -91,11 +93,11 @@ class Simulator
                 if ($this->month === 2) {//lineales
                     $occupiedPeriod = $this->occupiedFruitPerMonth->get($this->month);
                     $maleQtyFromLastPeriod = $this->simulatedPeriodsList->get($key - 1)->maleQty;
-                    $occupiedFruitPercentage = $occupiedPeriod[0] * $maleQtyFromLastPeriod + $occupiedPeriod[1];
+                    $occupiedFruitPercentage = $occupiedPeriod['mean'] * $maleQtyFromLastPeriod + $occupiedPeriod['deviation'];
                 } elseif ($this->month !== 10 && $this->occupiedFruitPerMonth->get($this->month) !== null) { //exponenciales
                     $occupiedPeriod = $this->occupiedFruitPerMonth->get($this->month);
                     $maleQtyFromLastPeriod = $this->simulatedPeriodsList->get($key - 1)->maleQty;
-                    $occupiedFruitPercentage = $occupiedPeriod[0] * log($maleQtyFromLastPeriod) + $occupiedPeriod[1];
+                    $occupiedFruitPercentage = $occupiedPeriod['mean'] * log($maleQtyFromLastPeriod) + $occupiedPeriod['deviation'];
                 } else {
                     $occupiedFruitPercentage = 0;
                 }
@@ -104,7 +106,7 @@ class Simulator
                 if ($this->month === 2) {//lineales
                     $occupiedPeriod = $this->insectsInCalyxPerMonth->get($this->month);
                     $maleQtyFromLastPeriod = $this->simulatedPeriodsList->get($key - 1)->maleQty;
-                    $insectsInCalyx = $occupiedPeriod[0] * $maleQtyFromLastPeriod + $occupiedPeriod[1];
+                    $insectsInCalyx = $occupiedPeriod['mean'] * $maleQtyFromLastPeriod + $occupiedPeriod['deviation'];
                 } else {
                     $insectsInCalyx = 0;
                 }
@@ -126,9 +128,9 @@ class Simulator
     //Calcular cantidad de Larvas
     private function calculateLarvaeQty()
     {
-        $temperature = $this->probability->getNormal(
-            $this->temperaturePerMonth->get($this->month)[0],
-            $this->temperaturePerMonth->get($this->month)[1]
+        $temperature = $this->probability->getUniform(
+            $this->temperaturePerMonth->get($this->month)['min'],
+            $this->temperaturePerMonth->get($this->month)['max']
         );
 
         if ($temperature >= 17 && $temperature <= 33) {
@@ -141,7 +143,9 @@ class Simulator
     private function calculateFemaleQty() : float
     {
         $maleQtyFromPeriod = $this->maleFlightPerMonth->get($this->month) ?? [0, 0];
-        return $this->probability->getNormal($maleQtyFromPeriod[0], $maleQtyFromPeriod[1]) * $this->FemaleMaleRatio();
+        return  isset($maleQtyFromPeriod['mean']) ?
+        $this->probability->getNormal($maleQtyFromPeriod['mean'], $maleQtyFromPeriod['deviation']) * $this->FemaleMaleRatio() :
+        0;
     }
 
     //Calcular Hembras con Huevos
@@ -168,7 +172,7 @@ class Simulator
         $ovisac = $this->probability->getUniform(3, 5);
 
         $eggs = 0;
-        for ($i=0; $i < $ovisac; $i++) {
+        for ($i = 0; $i < $ovisac; $i++) {
             $eggs += $this->probability->getUniform(100, 150);
         }
 
