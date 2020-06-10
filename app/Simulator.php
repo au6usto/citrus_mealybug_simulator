902ce3da9d5d5083f940376aa1159695b1defb0d
jesus <jemanuelpachecoo@gmail.com>
2020-06-10 17:25:06 -0300

    modifico valores funciones

--- app/Simulator.php
+++ app/Simulator.php
@@ -25,28 +25,27 @@ class Simulator
     private $temperature;
 
     private $simulatedPeriodsList;
-    
+
     public function __construct()
     {
         $this->probability = new ProbabilityNumber();
         $this->Gu = new PseudoRandomNumber(1);
 
         $this->periods = collect([
-            10 => ['name' => 'Primer Vuelo', 'month' => 'Octubre'],
-            11 => ['name' => 'Segundo Vuelo', 'month' => 'Noviembre/Diciembre'],
+            11 => ['name' => 'Primer Vuelo', 'month' => 'Octubre'],
             12 => ['name' => 'Segundo Vuelo', 'month' => 'Noviembre/Diciembre'],
-            1 => ['name' => 'Tercer Vuelo', 'month' => 'Enero'],
-            2 => ['name' => 'Cuarto Vuelo', 'month' => 'Febrero'],
-            3 => ['name' => 'Reducción de Población', 'month' => 'Marzo'],
-            4 => ['name' => 'Deceso', 'month' => 'Abril'],
-            5 => ['name' => 'Deceso', 'month' => 'Mayo'],
+            1 => ['name' => 'Segundo Vuelo', 'month' => 'Noviembre/Diciembre'],
+            2 => ['name' => 'Tercer Vuelo', 'month' => 'Enero'],
+            3 => ['name' => 'Cuarto Vuelo', 'month' => 'Febrero'],
+            4 => ['name' => 'Reducción de Población', 'month' => 'Marzo'],
+            5 => ['name' => 'Deceso', 'month' => 'Abril'],
+            6 => ['name' => 'Deceso', 'month' => 'Mayo'],
         ]);
 
         $this->simulatedPeriodsList = collect([]);
 
         //Temperaturas por mes
         $this->temperaturePerMonth = collect([
-            10 => ['min' => 15, 'max' => 29],
             11 => ['min' => 18, 'max' => 31],
             12 => ['min' => 20, 'max' => 32],
             1 => ['min' => 22,'max' => 32],
@@ -54,32 +53,42 @@ class Simulator
             3 => ['min' => 18, 'max' => 29],
             4 => ['min' => 14, 'max' => 26],
             5 => ['min' => 10, 'max' => 23],
+            6 => ['min' => 8, 'max' => 32],
         ]);
 
         //Vuelo de Machos por mes
         $this->maleFlightPerMonth = collect([
-            10 => ['mean' => 237.6, 'deviation' => 84.1],
-            11 => ['mean' => 1529.2, 'deviation' => 384.2],
+            11 => ['mean' => 237.6, 'deviation' => 84.1],
             12 => ['mean' => 1529.2, 'deviation' => 384.2],
-            1 => ['mean' => 1251.6, 'deviation' => 213.4],
-            2 => ['mean' => 404.4, 'deviation' => 77.5],
-            3 => ['mean' => 228.04, 'deviation' => 60.5]
+            1 => ['mean' => 1529.2, 'deviation' => 384.2],
+            2 => ['mean' => 1251.6, 'deviation' => 213.4],
+            3 => ['mean' => 404.4, 'deviation' => 77.5],
+            4 => ['mean' => 228.04, 'deviation' => 60.5]
         ]);
 
         //Frutos ocupados por mes
         $this->occupiedFruitPerMonth = collect([
-            11 => ['mean' => 13.14, 'deviation' => 3.9795],
-            12 => ['mean' => 13.14, 'deviation' => 3.9795],
-            1 => ['mean' => 5.635, 'deviation' => 22.545],
-            2 => ['mean' => 0.0005, 'deviation' => 0.215]
+            1 => ['a' => 13.47, 'b' => 3.9795], //enero => ln
+            2 => ['a' => 5.5655, 'b' => -22.545], // 1° quincena de febrero => ln
+            3 => ['a' => 0.008, 'b' => 8.5797], // 2° quincena de febrero
+            4 => ['a' => 0.008, 'b' => 13.806], // 1° quincena marzo
+            5 => ['a' => 0.0189, 'b' => 14.498], // 2° quincena marzo
+            6 => ['a' => 0.0114, 'b' => 10.27], // 1° quincena junio
         ]);
 
         //Insectos en cáliz por mes
         $this->insectsInCalyxPerMonth = collect([
-            11 => ['mean' => 0.0215, 'deviation' => 0.9913],
-            12 => ['mean' => 0.0215, 'deviation' => 0.9913],
-            1 => ['mean' => 0.0002, 'deviation' => 0.0892],
-            2 => ['mean' => 0.008, 'deviation' => 13.806]
+            1 => ['a' => 0.0215, 'b' => 0.9913], //enero
+            2 => ['a' => 0.00002, 'b' => 0.0892], // 1° quincena de febrero
+            3 => ['a' => 0.0005, 'b' => 0.0894], // 2° quincena de febrero
+            4 => ['a' => 0.0005, 'b' => 0.215], // 1° quincena marzo
+            5 => ['a' => 0.0009, 'b' => 0.3163], // 2° quincena marzo
+            6 => ['a' => 0.0007, 'b' => 0.3281], // 1° abril
+            7 => ['a' => 0.0002, 'b' => 0.1331], // 1° quincena junio
+        ]);
+
+        $this->relationOccupiedDamage = collect([
+            'a'=> 0,7911, 'b' => -3,5608
         ]);
     }
 
@@ -106,7 +115,7 @@ class Simulator
                 if ($this->month === 2) {//lineales
                     $occupiedPeriod = $this->occupiedFruitPerMonth->get($this->month);
                     //Cantidad de machos del período anterior
-                    $maleQtyFromLastPeriod = $this->simulatedPeriodsList->get($i - 1)->maleQty;
+                    $maleQtyFromLastPeriod = $this->simulatedPeriodsList->get($i - 2)->maleQty;
                     //Porcentaje de fruto ocupado
                     $occupiedFruitPercentage = $occupiedPeriod['mean'] * $maleQtyFromLastPeriod + $occupiedPeriod['deviation'];
                 } elseif ($this->month !== 10 && $this->occupiedFruitPerMonth->get($this->month) !== null) { //exponenciales
@@ -128,7 +137,7 @@ class Simulator
 
                 //Calculo total de huevos
                 $eggsTotal = $this->calcaultateEggsTotal() * $this->calculateImpregnatedFemale($femaleQty);
-            
+
                 $simulation = new SimulatedPeriod(
                     $period['name'],
                     $period['month'],
@@ -184,7 +193,7 @@ class Simulator
     {
         $this->Gu->generate();
         $Gu = $this->Gu->get(0);
-        
+
         $probs = collect([0.4, 0.8, 1]);
 
         $p = $probs->first(function ($value, $key) use ($Gu) {
